71afde1916d1b7bafa00457e6af37fd286a95493
asaf-bg <asafbg2@gmail.com>
2021-02-25 11:15:50 +0200

    first

---
+++ src/Components/Resume.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
+class Resume extends Component {
+  render() {
+
+    if(this.props.data){
+      var skillmessage = this.props.data.skillmessage;
+      var education = this.props.data.education.map(function(education){
+        return <div key={education.school}><h3>{education.school}</h3>
+        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        <p className="info">{education.description}</p></div>
+      })
+      var work = this.props.data.work.map(function(work){
+        return <div key={work.company}><h3>{work.company}</h3>
+            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+            <p style={{whiteSpace: 'pre-line',color:'white'}} >{work.description}</p>
+        </div>
+      })
+      var skills = this.props.data.skills.map(function(skills){
+        var className = 'bar-expand '+skills.name.toLowerCase();
+        return <li key={skills.name} >
+            <ProgressBar variant="danger" style={{height:"50%", backgroundColor:"white"}} animated now={skills.level} />
+            <em>{skills.name}</em></li>
+      })
+    }
+
+    return (
+      <section id="resume" >
+          <div className=" d-flex justify-content-md-center">
+              <div className="col col-lg-2">
+                  <h1><span style={{right:'10px'}}>Education</span></h1>
+              </div>
+              <div className="col-4">
+                  {education}
+              </div>
+          </div>
+          <div className=" d-flex justify-content-md-center">
+              <div className="col col-lg-2">
+                  <h1><span style={{right:'10px'}} className="text-left">Work</span></h1>
+              </div>
+              <div className="col-4">
+                  {work}
+              </div>
+          </div>
+          <div className=" d-flex justify-content-md-center">
+              <div className="col col-lg-2">
+                  <h1><span style={{right:'10px'}} className="text-left">Skills</span></h1>
+              </div>
+              <div className="col-4">
+                  <p>{skillmessage}
+                  </p>
+
+                  <div className="bars">
+                      <ul className="skills">
+                          {skills}
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
+
+
+
+   </section>
+    );
+  }
+}
+
+export default Resume;
---
+++ src/Components/Testimonials.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Testimonials extends Component {
+  render() {
+
+    if(this.props.data){
+      var testimonials = this.props.data.testimonials.map(function(testimonials){
+        return  <li key={testimonials.user}>
+            <blockquote>
+               <p>{testimonials.text}</p>
+               <cite>{testimonials.user}</cite>
+            </blockquote>
+         </li>
+      })
+    }
+
+    return (
+      <section id="testimonials">
+      <div className="d-flex justify-content-md-center">
+          <script id="6022e78fe44ae025f41ba356" src="https://dashboard.chatfuel.com/integration/fb-entry-point.js" async defer></script>
+
+          <div className="row">
+
+            <div className="two columns header-col">
+               <h1><span>Client Testimonials</span></h1>
+            </div>
+
+            <div className="ten columns flex-container">
+                  <ul className="slides">
+                      {testimonials}
+                  </ul>
+               </div>
+            </div>
+         </div>
+   </section>
+    );
+  }
+}
+
+export default Testimonials;
